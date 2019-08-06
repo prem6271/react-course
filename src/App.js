80aec83ad6e9e7ec169497dfc278137183c03a6d
Prem <prem6271@gmail.com>
2019-08-06 16:50:12 +0530

    Implemented functions and Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -10,14 +10,13 @@ import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
-class App extends Component {
-  state = {
-    user:{},
-    users: [],
-    loading: false,
-    showClear: false,
-    alert: null
-  }
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [showClear, setshowClear] = useState(false);
+  const [alert, setAlert] = useState(null);
+
   /* async componentDidMount() {
     this.setState({loading: true});
     const res = await axios.get(`https://api.github.com/users?
@@ -28,75 +27,71 @@ class App extends Component {
   }
   */
   // Setting State
-  searchingUsers = async user => {
-    this.setState({loading: true});
+  const searchingUsers = async user => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/search/users?q=${user}&
       client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
       CLIENT_SECRET=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     //console.log(res.data);
-    this.setState({users : res.data.items, loading: false, showClear: true});
+    setUsers(res.data.items);
+    setLoading(false);
+    setshowClear(true);
   }
 
-  getUser = async username => {
-    this.setState({loading: true});
+  const getUser = async username => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/users/${username}?
       client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
       CLIENT_SECRET=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     //console.log(res.data);
-    this.setState({user : res.data, loading: false});
+    setUser(res.data);
+    setLoading(false);
   }
 
   // Clearing State
-  clearingUsers = () =>
-    this.setState( {
-      users: [],
-      loading: false,
-      showClear: false,
-      alert: null
-      }
-    );
+  const clearingUsers = () => {
+    setUsers([]);
+    setLoading(false);
+    setshowClear(false);
+    setAlert(null);
+  };
 
-  settingAlert = (message, type) => {
-    this.setState( {
-        alert: { message: message, type : type }
-      }
-    );
-    setTimeout(function(){
-             this.setState({alert:null});
-    }.bind(this),5000);
+  const settingAlert = (message, type) => {
+    setAlert({message: message, type : type});
+    setTimeout(() => setAlert(null),5000);
   }
-  render() {
-    return (
-      // looks like html, but it is JSX i.e. JS syntax extension
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            {
-              this.state.alert && <Alert alert={this.state.alert}/>
-            }
-            <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search
-                    searchUsers={this.searchingUsers}
-                    clearUsers={this.clearingUsers}
-                    showClear={this.state.showClear}
-                    setAlert={this.settingAlert}
-                  />
-                  <Users loading={this.state.loading} users={this.state.users}/>
-                </Fragment>
-              )} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' render={props => (
-                <User {...props} loading={this.state.loading} user={this.state.user} getUser={this.getUser}/>
-              )} />
-            </Switch>
-          </div>
+
+  return (
+    // looks like html, but it is JSX i.e. JS syntax extension
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          {
+            alert && <Alert alert={alert}/>
+          }
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                <Search
+                  searchUsers={searchingUsers}
+                  clearUsers={clearingUsers}
+                  showClear={showClear}
+                  setAlert={settingAlert}
+                />
+                <Users loading={loading} users={users}/>
+              </Fragment>
+            )} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/user/:login' render={props => (
+              <User {...props} loading={loading} user={user} getUser={getUser}/>
+            )} />
+          </Switch>
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
+
 }
 
 export default App;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,38 +1,41 @@
-import React, {Component, Fragment} from 'react';
+import React, {useEffect, Fragment} from 'react';
 import Spinner from '../layout/Spinner';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-class User extends Component {
-  componentDidMount() {
-    // login comes from props
-    this.props.getUser(this.props.match.params.login);
-  }
-  
-  render () {
-    const {
-      name,
-      avatar_url,
-      location,
-      html_url,
-      hireable
-    } = this.props.user;
-    if (this.props.loading) {
-      return <Spinner />
-    } else {
-      return (
-        <Fragment>
-          <Link to='/' className="btn btn-light">
-            Back to Search
-          </Link>
-          Hireable:{' '}
-          {hireable ? (
-            <i className='fas fa-check text-success' />
-          ) : (
-            <i className='fas fa-times-circle text-danger' />
-          )}
-        </Fragment>
-      )
-    }
+const User = ({user, loading, getUser, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    // eslint-disable-next-line
+  }, []);
+
+  const {
+    hireable
+  } = user;
+
+  if (loading) {
+    return <Spinner />
+  } else {
+    return (
+      <Fragment>
+        <Link to='/' className="btn btn-light">
+          Back to Search
+        </Link>
+        Hireable:{' '}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+      </Fragment>
+    )
   }
 }
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+}
+
 export default User;
